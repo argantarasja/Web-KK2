2ed6568a0b9597d686a1bc6b3be186a67e77000a
Argantara Ramadhani <ramadhaniargantara@gmail.com>
2025-10-17 11:16:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showContent(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    const target = document.getElementById(sectionId);
+    if (target) {
+        target.classList.add('active');
+    }
+}
